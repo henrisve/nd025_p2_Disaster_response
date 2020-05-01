ae88b45135a7e51ef2bff87e0013baba49d0631c
Henrik <henrisve@gmail.com>
2020-05-01 12:45:28 -0400

    updated run.py with correct filenames

--- app/run.py
+++ app/run.py
@@ -26,11 +26,11 @@ def tokenize(text):
     return clean_tokens
 
 # load data
-engine = create_engine('sqlite:///../data/YourDatabaseName.db')
-df = pd.read_sql_table('YourTableName', engine)
+engine = create_engine('sqlite:///../data/disaster_response.db')
+df = pd.read_sql_table('table1', engine)
 
 # load model
-model = joblib.load("../models/your_model_name.pkl")
+model = joblib.load("../models/test.pkl")
 
 
 # index webpage displays cool visuals and receives user input text for model
